265de88192cd84923756ffb15a659d1ffbf057e6
schoeneBiene <schoeneBiene@users.noreply.github.com>
2024-06-01 06:41:28 +0000

    deploy: b85b90696d1d1f6f2fae5b7ccd3dddbdbf745818

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Kanagawa" /* The official name of your theme. */,
+        scss: "kanagawa" /* The name of your base scss file. */,
+        repo: "kanagawa" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
